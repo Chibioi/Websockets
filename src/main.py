fc169c1e6b24c276af2539511470279df2a4d24d
Chibioi <ezehstanley2002@gmail.com>
2025-05-26 19:04:31 +0100

    Updated

---
+++ src/main.py
@@ -0,0 +1,60 @@
+import socket
+import select  # Provides access to the select() system call
+# The select() system call monitor many file descriptors e.g sockets and wait to see if one or more of them are ready for some I/O operations
+
+
+tcp_ip = "127.0.0.1"
+tcp_port = 5010
+buffer_size = 1024 * 1024
+
+default_http_response = b"""<HTML><HEAD><meta http-equiv="content-type" content="text/html;charset=utf-8">\r\n
+<TITLE>200 OK</TITLE></HEAD><BODY>\r\n
+<H1>200 OK</H1>\r\n
+Welcome to the default.\r\n
+</BODY></HTML>\r\n\r\n"""
+
+
+def main():
+    tcp_socket = socket.socket(
+        socket.AF_INET, socket.SOCK_STREAM
+    )  # Address family is the AF.INET and the socket type is SOCK_STREAM
+    tcp_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    tcp_socket.bind((tcp_ip, tcp_port))
+    tcp_socket.listen(1)
+    print("Listening on port: ", tcp_port)
+    input_sockets = [tcp_socket]  # Reading
+    output_sockets = []  # Writing
+    xlist = []  # For exceptional conditions e.g can indicate out-of-bounds data
+
+    while True:
+        # Get the sockets that are ready to be read (the first three of the tuple)
+        readable_sockets = select.select(
+            input_sockets, output_sockets, xlist, 5
+        )[
+            0
+        ]  # Involving a system call (select()). Returns a list of sockets that are ready for Reading, Writing and Exceptions. Because of the [0] it returns only the first index of the tuple which is the sockets available for Reading which is assigned to the variable readable_sockets.
+        for ready_socket in readable_sockets:
+            # Make sure is not already closed
+            if (
+                ready_socket.fileno == -1
+            ):  # returns the underlying file descriptor (e.g sockets in this case) of the stream if it exists. An OSError is raised if the IO object does not use a file descriptor.
+                continue
+            if ready_socket == tcp_socket:
+                print("Handling main door socket")
+                HandleNewConnection(tcp_socket, input_sockets)
+            else:
+                print("Handling regular socket read")
+                HandleRequest(ready_socket, input_sockets)
+
+
+def HandleNewConnection(main_door_socket, input_sockets):
+    # When we get a connection on the main socket, we want to accept the new
+    # connection and add it to our input socket list. When we loop back around,
+    # that socket will be ready to read from.
+    client_socket, client_address = main_door_socket.accept()
+    print("New socket", client_socket.fileno(), "from address:", client_address)
+    input_sockets.append(client_socket)
+
+
+def HandleRequest(client_socket, input_sockets):
+    pass
